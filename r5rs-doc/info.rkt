86155051b3985181d26151737f870db15b323ca9
Philip McGrath <philip@philipmcgrath.com>
2021-10-16 16:26:33 -0400

    r5rs-doc/info.rkt: note lack of license metadata (needs SchemeReport)
    The license used for the RNRS documents does not yet have an SPDX
    license identifier, so we can't add accurate license metadata to
    this package yet. For now, add a comment explaining the situation.
    
    Also, add a note about this license in the LICENSE file.
    
    Related to spdx/license-list-XML#1340
    Related to racket/racket#3760

--- r5rs-doc/info.rkt
+++ r5rs-doc/info.rkt
@@ -14,3 +14,11 @@
 (define pkg-desc "documentation part of \"r5rs\"")
 
 (define pkg-authors '(mflatt))
+
+;; TODO:
+;; Once <https://tools.spdx.org/app/license_requests/126/>
+;; is accepted (see <https://github.com/spdx/license-list-XML/issues/1340>),
+;; uncomment this:
+#;
+(define license
+  '(SchemeReport AND (Apache-2.0 OR MIT)))
